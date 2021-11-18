2da3da336da7aadf4cf8a8b5ba712a0937e761cb
Harvinder Singh Dhillon <72372197+dhillon1057@users.noreply.github.com>
2021-11-18 23:45:53 +0530

    Add files via upload

---
+++ blog/app/includes/footer.php
@@ -0,0 +1,22 @@
+<div class="footer">
+        <div class="footer-content">
+            <div class="footer-about">
+                <h1 class="logo-text"><span>Blog</span>Post</h1>
+                <p>
+                    BlogPost is a one stop website for all your reading and blog posting.
+                    A variety of blogs can be found here according to your interests.
+                </p>
+            </div>
+            <div class="footer-contact">
+                <h2>Contact Us</h2>
+                <br>
+                <span><i class="fas fa-phone"></i> &nbsp; 123-456-7890</span>
+                <span><i class="fas fa-envelope"></i> &nbsp; info@blogpost.com</span>
+                <div class="social">
+                    <a href="#" class="fab fa-facebook"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                </div>
+            </div>
+        </div>
+    </div>
